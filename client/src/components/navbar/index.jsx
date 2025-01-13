b139fa41a40f3fedc39c58032ef59a1edccfa3fd
unknown <vishnuravichandran.28@gmail.com>
2025-01-13 12:08:06 +0530

    added color in the navbar

--- client/src/components/navbar/index.jsx
+++ client/src/components/navbar/index.jsx
@@ -51,7 +51,7 @@ export default function Navbar() {
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <a onClick={handleLogout}>Logout</a>
+                  <a onClick={handleLogout} style={{ color: "#FF735C" }}>Logout</a>
                 </li>
               </ul>
             )}
